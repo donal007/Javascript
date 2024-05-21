fef361f75a9a56141328cece3e92e21c2e9306a7
donal007 <150226333+donal007@users.noreply.github.com>
2024-05-21 14:52:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Array of object 
+let bsc =[
+    {
+    fname: "Donal",
+    Age : 19,
+},
+    {
+    fname: "Anaj",
+    Age : 20,
+    },
+    {
+    fname: "Ashwin",
+    Age : 21,
+    },
+
+];
+console.log(bsc[0].Age);
+console.log(bsc[2].Age);
